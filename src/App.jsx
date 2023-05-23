ecc97848203be867b0134025b91c36585cd89b40
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-23 10:35:13 -0300

    se agrega maquetado y footer al final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
-  return <>
-  </>;
+  return (
+    <>
+      <Container className="my-5 main">
+        <h1>Lista de tareas</h1>
+        <hr />
+      </Container>
+      <footer className="bg-dark text-center text-light py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;

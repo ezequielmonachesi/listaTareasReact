c3824388855cd776d799c1a7b06a57505c5abb95
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-23 10:37:36 -0300

    se agrega fondo y además se estilisa el h1 y h3

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <>
       <Container className="my-5 main">
-        <h1>Lista de tareas</h1>
-        <hr />
+        <section className="text-light">
+          <h1 className="text-center display-4">Lista de tareas</h1>
+          <hr />
+        </section>
       </Container>
       <footer className="bg-dark text-center text-light py-4">
         <p>&copy; Todos los derechos reservados</p>

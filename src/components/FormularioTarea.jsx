b21d18589389a05dd93ec5d0d9bdd720cddc0314
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-26 16:07:18 -0300

    creando tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,17 +1,34 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTareas([...tareas, tarea]);
+    setTarea("");
+  };
+
   return (
-   <>
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="Ingrese una tarea" />
-        <Button variant="primary">Agregar</Button>
-      </Form.Group>
-    </Form>
-    <ListaTareas/>
-   </>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value = {tarea}
+          />
+          <Button variant="primary" type="submit">
+            Agregar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas />
+    </>
   );
 };
 

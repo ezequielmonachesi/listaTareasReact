fa635e64a2b2dbb41ebedbc583d022aecca71436
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-30 10:49:40 -0300

    se agrega informacion al localstorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,11 +1,20 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const FormularioTarea = () => {
+  
+  let tareasDelLocalStorage = JSON.parse(localStorage.getItem('listaTareas')) || [];
   const [tarea, setTarea] = useState("");
-  const [tareas, setTareas] = useState([]);
+  const [tareas, setTareas] = useState(tareasDelLocalStorage);
 
+  //ciclo de vida
+  useEffect(()=>{
+    localStorage.setItem('listaTareas', JSON.stringify(tareas))
+  },[tareas]);
+//con corchetes vacio solo se ejectura en montaje
+// sin corchetes se ejecuta en montaje y actualizacion
+// cuando se asigna una variable se ejecuta el montaje y actualizacion en esa variable
   const handleSubmit = (e) => {
     e.preventDefault();
     setTareas([...tareas, tarea]);
